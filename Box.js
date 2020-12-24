7597f0eeccee6beff4eb9e87dcd8727ec33938c8
Sreeyashasvi <74099332+Sreeyashasvi@users.noreply.github.com>
2020-12-24 13:13:46 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+class Box{
+    constructor(x, y,r) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, r/2, r/2, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos=this.body.position;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+}
+  
